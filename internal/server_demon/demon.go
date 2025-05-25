3888872fdde2cfdf7f2212a3c26b5ab8d9a75963
samarskiyad <samarskiyad@student.bmstu.ru>
2025-05-25 12:27:11 +0300

    Add utils

---
+++ internal/server_demon/demon.go
@@ -0,0 +1,98 @@
+package server_demon
+
+import (
+	"context"
+	"log"
+	"paymentgo/internal/cmd/auth"
+	"paymentgo/internal/cmd/yoomoney"
+	dto "paymentgo/internal/entity"
+	"paymentgo/internal/repository"
+	"paymentgo/internal/usecase/service"
+	db "paymentgo/utils/connector"
+	"time"
+
+	"go.uber.org/zap"
+)
+
+// PaymentDemon Структура демона для проверки счетов
+type PaymentDemon struct {
+	service       service.PaymentService
+	repo          repository.PaymentRepository
+	paymentClient *yoomoney.YooMoneyClient
+	paymentsQueue *db.LockFreeQueue
+	authClient    *auth.AuthClient
+	logger        *zap.Logger
+}
+
+// NewPaymentDemon Создание экземпляра демона
+func NewPaymentDemon(service service.PaymentService, repo repository.PaymentRepository, paymentClient *yoomoney.YooMoneyClient, paymentQueue *db.LockFreeQueue, logger *zap.Logger, authClient *clients.AuthClient) *PaymentDemon {
+	return &PaymentDemon{
+		service:       service,
+		paymentClient: paymentClient,
+		paymentsQueue: paymentQueue,
+		logger:        logger,
+		authClient:    authClient,
+	}
+}
+
+// Start Бесконечный цикл проверки счетов
+func (d PaymentDemon) Start(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			log.Println("Payment demon stopped")
+			return
+		default:
+			payment, ok := d.paymentsQueue.Dequeue()
+			if !ok {
+				time.Sleep(1 * time.Second)
+				continue
+			}
+
+			status, err := d.service.GetPayment(ctx, payment.ID)
+			if err != nil {
+				d.paymentsQueue.Enqueue(payment)
+				d.logger.Error("Failed to check payment status", zap.String("payment_id", payment.ID), zap.Error(err))
+				continue
+			}
+
+			switch status {
+			case "success":
+				receiverData, err := d.authClient.GetUserById(ctx, payment.ToUserID)
+				if err != nil {
+					d.paymentsQueue.Enqueue(payment)
+					d.logger.Error("Failed to get receiver", zap.String("user_id", payment.ToUserID), zap.Error(err))
+				}
+
+				receiver := receiverData.YoomoneyId
+
+				err = d.repo.UpdatePaymentStatus(ctx, payment.ID, dto.StatusComplete)
+				if err != nil {
+					d.paymentsQueue.Enqueue(payment)
+					d.logger.Error("Failed to update payment status", zap.String("payment_id", payment.ID), zap.Error(err))
+				}
+
+				paymentStatus, err := d.paymentClient.CreateTransfer(&payment, receiver)
+				if err != nil {
+					err = d.repo.UpdatePaymentStatus(ctx, payment.ID, dto.StatusSuccess)
+					if err != nil {
+						d.logger.Error("Failed to update payment status", zap.String("payment_id", payment.ID), zap.Error(err))
+					}
+					d.paymentsQueue.Enqueue(payment)
+					d.logger.Error("Failed to create new transfer", zap.String("original_payment_id", payment.ID), zap.Error(err))
+				} else {
+					d.logger.Info("New transfer created", zap.String("status", paymentStatus))
+				}
+
+			case "pending", "failed":
+				d.paymentsQueue.Enqueue(payment)
+				d.logger.Info("Re-enqueued payment for further processing", zap.String("payment_id", payment.ID))
+			case "complete":
+				d.logger.Info("Payment complete", zap.String("payment_id", payment.ID))
+			default:
+				d.paymentsQueue.Enqueue(payment)
+				d.logger.Warn("Unexpected payment status", zap.String("payment_id", payment.ID), zap.String("status", status))
+			}
+		}
+	}
+}
---
+++ utils/connector/queue.go
@@ -0,0 +1,74 @@
+package connector
+
+import (
+	dto "paymentgo/internal/entity"
+	"sync/atomic"
+	"unsafe"
+)
+
+// Queue асинхронная очередь
+type Queue interface {
+	Enqueue(element dto.Payment)
+	EnqueueList(data []dto.Payment)
+	Dequeue() (dto.Payment, bool)
+}
+
+type QueueNode struct {
+	expression dto.Payment
+	next       unsafe.Pointer
+}
+
+type LockFreeQueue struct {
+	head unsafe.Pointer
+	tail unsafe.Pointer
+}
+
+func NewPaymentsQueue() *LockFreeQueue {
+	dummy := &QueueNode{}
+	return &LockFreeQueue{
+		head: unsafe.Pointer(dummy),
+		tail: unsafe.Pointer(dummy),
+	}
+}
+
+func (q *LockFreeQueue) Enqueue(element dto.Payment) {
+	newNode := &QueueNode{expression: element}
+
+	for {
+		tail := atomic.LoadPointer(&q.tail)
+		next := atomic.LoadPointer(&((*QueueNode)(tail)).next)
+
+		if tail == atomic.LoadPointer(&q.tail) {
+			if next == nil {
+				if atomic.CompareAndSwapPointer(&((*QueueNode)(tail)).next, nil, unsafe.Pointer(newNode)) {
+					atomic.CompareAndSwapPointer(&q.tail, tail, unsafe.Pointer(newNode))
+					return
+				}
+			} else {
+				atomic.CompareAndSwapPointer(&q.tail, tail, next)
+			}
+		}
+	}
+}
+
+func (q *LockFreeQueue) EnqueueList(data []dto.Payment) {
+	for _, expr := range data {
+		q.Enqueue(expr)
+	}
+}
+
+func (q *LockFreeQueue) Dequeue() (dto.Payment, bool) {
+	for {
+		head := atomic.LoadPointer(&q.head)
+		next := atomic.LoadPointer(&((*QueueNode)(head)).next)
+
+		if head == atomic.LoadPointer(&q.head) {
+			if next == nil {
+				return dto.Payment{}, false
+			}
+			if atomic.CompareAndSwapPointer(&q.head, head, next) {
+				return (*QueueNode)(next).expression, true
+			}
+		}
+	}
+}
